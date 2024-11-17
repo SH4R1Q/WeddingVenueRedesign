72d530509a722d000b5745668a0ca52adef800d2
Amit Rawat <68739013+Rookie0208@users.noreply.github.com>
2024-11-17 17:27:59 +0530

    update profile page

--- FrontDeploy/src/components/UserProfileSidebar.tsx
+++ FrontDeploy/src/components/UserProfileSidebar.tsx
@@ -12,7 +12,7 @@ interface Props {
   id?: string | undefined;
 }
 
-const UserSidebar: React.FC<Props> = () => {
+const UserSidebar: React.FC<Props> = ({ yourName, profile, phone, email }) => {
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
 
@@ -37,27 +37,27 @@ const UserSidebar: React.FC<Props> = () => {
   };
 
   return (
-    <div className="flex md:h-screen"  >
-      <div className=" w-64 sm:w-80 p-6  rounded-lg shadow-lg md:shadow-none md:rounded-none" style={{ backgroundColor: 'rgb(254,234,232)' }}>
+    <div className="flex md:h-screen bg-gray-100">
+      <div className="bg-blue-900 w-64 sm:w-80 p-6 flex flex-col items-center rounded-lg shadow-lg md:shadow-none md:rounded-none">
         
-        {/* Profile Picture
+        {/* Profile Picture */}
         <div className="w-28 h-28 mt-6 mb-4 rounded-full overflow-hidden border-2 border-white shadow-lg">
           <img src={profile} alt="Profile" className="w-full h-full object-cover" />
         </div>
 
         {/* User Information */}
-        {/* <div className="text-center mb-8">
+        <div className="text-center mb-8">
           <h3 className="text-2xl font-semibold text-white">{yourName}</h3>
           <p className="text-gray-200">{email}</p>
           <p className="text-gray-200">{phone}</p>
-        </div> */}
+        </div>
 
         {/* Navigation Links */}
-        <nav className="w-full flex flex-col space-y-4">
+        <nav className="mt-8 w-full flex flex-col space-y-4">
           {['My venues', 'My services', 'List a new service', 'Logout'].map(tab => (
             <button
               key={tab}
-              className="text-lg font-medium text-white-500 w-full py-3 rounded-lg hover:text-white transition-colors duration-300" style={{backgroundColor: 'rgb(231,47,118)'}}
+              className="text-lg font-medium text-gray-200 w-full py-3 rounded-lg bg-blue-800 hover:bg-blue-700 hover:text-white transition-colors duration-300"
               onClick={() => handleTabClick(tab)}
             >
               {tab}
