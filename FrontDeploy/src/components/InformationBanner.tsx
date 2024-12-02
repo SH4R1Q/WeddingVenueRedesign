ecfba5ec62a2542fc38e5cd3bc3af872fc46ea40
Amit Rawat <68739013+Rookie0208@users.noreply.github.com>
2024-12-02 23:53:54 +0530

    remove phone number in home page

--- FrontDeploy/src/components/InformationBanner.tsx
+++ FrontDeploy/src/components/InformationBanner.tsx
@@ -44,20 +44,20 @@ const InformationBanner = () => (
       <div className="flex-1 space-y-6 border-r border-gray-500 pr-6 md:pr-10">
         {/* Contact Information */}
         <div className="flex items-center text-lg font-semibold space-x-3">
-          <Phone className="w-6 h-6 text-yellow-300" strokeWidth={2.5} />
+          {/* <Phone className="w-6 h-6 text-yellow-300" strokeWidth={2.5} /> */}
           <span className="text-left">
-            <span className="uppercase text-sm text-gray-300 tracking-wide">Call Us:</span>
+            {/* <span className="uppercase text-sm text-gray-300 tracking-wide">Call Us:</span>
             <a href="tel:+918076207112" className="text-yellow-300 text-2xl ml-2 font-bold">
               +91 8076207112
-            </a>
+            </a> */}
           </span>
         </div>
         {/* Helpline Note */}
-        <div className="text-left text-sm italic opacity-80 text-gray-400">24-hour helpline</div>
+        {/* <div className="text-left text-sm italic opacity-80 text-gray-400">24-hour helpline</div> */}
         {/* Divider Text */}
-        <div className="text-lg font-semibold text-left text-gray-300">or</div>
+        {/* <div className="text-lg font-semibold text-left text-gray-300">or</div> */}
         {/* Call Back Offer */}
-        <div className="text-left text-lg font-semibold">
+        <div className="text-left text-4xl font-semibold">
           <span className="animate-pulse">Have us call you for up to </span>
           <span className="text-yellow-300 text-2xl font-bold">30%</span> discount
         </div>
