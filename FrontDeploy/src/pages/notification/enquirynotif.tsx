ce090689e27e9ee6d065921b58259f3f19e8f960
Amit Rawat <amit.rawat@via.com>
2025-03-23 21:04:16 +0530

    update build error

--- FrontDeploy/src/pages/notification/enquirynotif.tsx
+++ FrontDeploy/src/pages/notification/enquirynotif.tsx
@@ -79,7 +79,7 @@ const EnquiryNotif = () => {
     <>
       <NavBar />
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {data?.map((user: any, index: number) => ( //  ye vala error resolve krdo
+      {(data as any[])?.map((user:any, index: number)=>(
         
           <div
             key={index}
